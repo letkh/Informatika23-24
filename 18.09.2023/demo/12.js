c79ce473e16f8dd1a8c127394e0a912848d58053
letkh <letkhi246@gmail.com>
2023-09-16 23:58:35 +0300

    +12

---
+++ 18.09.2023/demo/12.js
@@ -0,0 +1,34 @@
+function sum(str) { //функция для счета суммы чисел
+    var sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        sum += parseInt(str[i])
+    }
+    return sum;
+}
+
+for (let n = 3; n < 10000; n++) { //цикл для обработки n
+  let str = `5${"2".repeat(n)}`;
+  //console.log(str)
+  while (str.includes("52") || str.includes("2222") || str.includes("1122")) { //обработка числа
+    if (str.includes("52")) {
+    str = str.replace("52", "11");
+      //console.log(str)
+    }
+    if (str.includes("2222")) {
+    str = str.replace("2222", "5");
+      //console.log(str);
+    }
+    if (str.includes("1122")) {
+    str = str.replace("1122", "25");
+      //console.log(str);
+    }
+  }
+  //console.log(sum(str))
+  if (sum(str) == 64) { //проверка на сумму чисел
+    console.log(str);
+  }
+}
+console.log('Цикл закончен')
+
+
+//Ответ: 1111111111111111111111111111111111111111111111111111111111111111
---
+++ 18.09.2023/demo/12.pas
@@ -0,0 +1,7 @@
+﻿// 12 Задание
+
+begin
+  for var n = 3 to 10000 do begin
+    while 
+  end;
+end.
